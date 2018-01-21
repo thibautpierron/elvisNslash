7b3aa938e09133393896e900544b9263b676a0fc
Adrien BOSSI <bossi.adrien@gmail.com>
2018-01-21 09:46:25 +0100

    add Talents OK

--- Assets/Scripts/Talents.cs
+++ Assets/Scripts/Talents.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class Talents : MonoBehaviour {
 
-	public Text		description;
+	public Text			description;
+	private Sort		actif;
+	public MayaSorts	player;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +15,33 @@ public class Talents : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKeyDown("1") && actif)
+			SetPosition(actif, 0);
+		if (Input.GetKeyDown("2") && actif)
+			SetPosition(actif, 1);
+		if (Input.GetKeyDown("3") && actif)
+			SetPosition(actif, 2);
+		if (Input.GetKeyDown("4") && actif)
+			SetPosition(actif, 3);
 	}
 
 	public void SetDescription(Sort sort) {
 		description.text = sort.Info();
+		SetActif(sort);
+	}
+
+	public void SetActif(Sort s) {
+		actif = s;
+	}
+
+	public void SetPosition(Sort sort, int position) {
+		for (int i = 0 ; i < 4 ; i++)
+		{
+			if (i == position)
+				player.sorts[i] = sort;
+			else if (player.sorts[i] == sort)
+				player.sorts[i] = null;
+		}
 	}
 }
